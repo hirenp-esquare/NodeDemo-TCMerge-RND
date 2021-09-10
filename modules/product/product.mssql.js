195722f18cc4ad0e71aa1417204c7aad56bb575d
hirenp-waferwire <52193642+hirenp-waferwire@users.noreply.github.com>
2021-09-10 16:46:08 +0530

    Temp

--- modules/product/product.mssql.js
+++
@@ -1,9 +0,0 @@
-const mssqlcon = require('../../dbconnection');
-class ProductMSSql { 
-   async getAllProducts() {
-    const conn = await mssqlcon.getConnection();
-    const res = await conn.request().query('select * from product');
-    return res.recordset;
-  }
-}
-module.exports = new ProductMSSql();
